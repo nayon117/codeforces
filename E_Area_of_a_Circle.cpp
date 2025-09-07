7b57f80cf797d88be8cbfbfee2b01560134c5f2d
nayon117 <hasibul.nayon1@gmail.com>
2025-09-07 18:52:40 +0600

    solve area of a circle problem

---
+++ E_Area_of_a_Circle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve () {
+   double n; cin >> n;
+   cout << fixed << setprecision(9) << (3.141592653 * (n * n)) << '\n';  
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    solve ();
+    return 0;
+}
