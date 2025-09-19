6a2b66ca0ceedfdf2c1a951e5895ece532f98802
nayon117 <hasibul.nayon1@gmail.com>
2025-09-19 18:39:12 +0600

    solve tree number problem

---
+++ Z_Three_Numbers.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve () {
+   int k, s; cin >> k >> s;
+   int cnt = 0;
+   for (int x = 0; x <= k; x++) {
+    for (int y = 0; y <= k; y++) {
+        int z = s - (x + y);
+        if(z >= 0 && z <= k) cnt++;   
+    }
+   }
+   cout << cnt << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    solve ();
+    return 0;
+}
