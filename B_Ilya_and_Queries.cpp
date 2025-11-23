a5889d2ea7cdfc89ce0a1b98504c5c92054ff71c
nayon117 <hasibul.nayon1@gmail.com>
2025-11-23 17:51:37 +0600

    solve ilya and queries problem

---
+++ B_Ilya_and_Queries.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    vector<int> pref(n, 0);
+    for (int i = 1; i < n; i++) {
+        pref[i] = pref[i-1] + (s[i] == s[i-1]);
+    }
+
+    int m;
+    cin >> m;
+    while (m--) {
+        int l, r;
+        cin >> l >> r;
+        cout << pref[r-1] - pref[l-1] << "\n";
+    }
+}
