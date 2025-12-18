b81b66738a182cc22c0ca13fd064106fc4324e03
nayon117 <hasibul.nayon1@gmail.com>
2025-12-18 18:14:26 +0600

    solve design tutorial problem

---
+++ A_Design_Tutorial_Learn_from_Math.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve () {
+    int n; cin >> n;
+    if(n % 2 == 0) cout << 4 << " " << n - 4;
+    else cout << 9 << " " << n - 9;
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    solve ();
+    return 0;
+}
