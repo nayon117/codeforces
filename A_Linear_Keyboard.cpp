190d1e71b730b1b3d83e96305cd24b3252e7d709
nayon117 <hasibul.nayon1@gmail.com>
2026-03-04 15:47:48 +0600

    solve linear keyboard problem

---
+++ A_Linear_Keyboard.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        string keyboard, s;
+        cin >> keyboard >> s;
+
+        vector<int> pos(26);
+        for(int i = 0; i < 26; i++)
+            pos[keyboard[i] - 'a'] = i;
+
+        int ans = 0;
+        for(int i = 1; i < (int)s.size(); i++){
+            ans += abs(pos[s[i] - 'a'] - pos[s[i - 1] - 'a']);
+        }
+
+        cout << ans << '\n';
+    }
+}
