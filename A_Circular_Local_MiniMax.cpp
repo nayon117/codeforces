573de0a9619e9090f9e51a6989a6fdb09b9dd1e9
nayon117 <hasibul.nayon1@gmail.com>
2026-03-26 17:44:53 +0600

    solve circular min max problem

--- A_Circular_Local_MiniMax.cpp
+++ A_Circular_Local_MiniMax.cpp
@@ -1,38 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
 
-void solve () {
-    int n; cin >> n;
-    vector<long long> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    sort(a.begin(), a.end());
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
 
-    unordered_map<long long,int> mp;
-    int mx = 0;
-    for(auto x : a) mx = max(mx, ++mp[x]);
+        sort(a.begin(), a.end());
 
-    if(mx > n / 2){
-        cout << "NO\n";
-        continue;
+        if (n % 2)
+        {
+            cout << "NO\n";
+            continue;
+        }
+
+        vector<long long> b(n);
+        int mid = n / 2;
+
+        for (int i = 0; i < mid; i++)
+        {
+            b[2 * i] = a[i];
+            b[2 * i + 1] = a[i + mid];
+        }
+
+        bool ok = true;
+        for (int i = 0; i < n; i++)
+        {
+            long long l = b[(i - 1 + n) % n];
+            long long r = b[(i + 1) % n];
+            if (!((b[i] > l && b[i] > r) || (b[i] < l && b[i] < r)))
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        if (!ok)
+        {
+            cout << "NO\n";
+        }
+        else
+        {
+            cout << "YES\n";
+            for (auto x : b)
+                cout << x << " ";
+            cout << "\n";
+        }
     }
-
-    vector<long long> b(n);
-    for(int i = 0; i < n; i++){
-        b[i] = a[(2LL * i) % n];
-    }
-
-    cout << "YES\n";
-    for(int i = 0; i < n; i++){
-        cout << b[i] << " ";
-    }
-    cout << "\n";  
-}
-
-int main() {
-    ios::sync_with_stdio(false);cin.tie(nullptr);
-    int t; cin >> t;
-    while(t--) solve ();
-    return 0;
 }
